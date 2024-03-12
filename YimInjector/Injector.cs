52fa624479230eda003c3b30fb1d07c69736b6d8
Remigiusz Schoida <inxaneninja@gmail.com>
2024-03-12 13:23:13 +0100

    Initial commit

---
+++ YimInjector/Injector.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YimInjector
+{
+    public class Injector
+    {
+        static readonly IntPtr INTPTR_ZERO = (IntPtr)0;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr OpenProcess(uint dwDesiredAccess, int bInheritHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, IntPtr dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, IntPtr dwStackSize, IntPtr lpStartAddress,
+            IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        public static bool Inject(Process process, string dllPath)
+        {
+            uint processId = (uint)process.Id;
+            IntPtr hndProc = OpenProcess((0x2 | 0x8 | 0x10 | 0x20 | 0x400), 1, processId);
+            if (hndProc == INTPTR_ZERO)
+            {
+                return false;
+            }
+            IntPtr loadLibraryAddress = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+            if (loadLibraryAddress == INTPTR_ZERO)
+            {
+                return false;
+            }
+            IntPtr address = VirtualAllocEx(hndProc, (IntPtr)null, (IntPtr)dllPath.Length, (0x1000 | 0x2000), 0x40);
+            if (address == INTPTR_ZERO)
+            {
+                return false;
+            }
+            byte[] bytes = Encoding.ASCII.GetBytes(dllPath);
+            if (WriteProcessMemory(hndProc, address, bytes, (uint)bytes.Length, 0) == 0)
+            {
+                return false;
+            }
+            if (CreateRemoteThread(hndProc, (IntPtr)null, INTPTR_ZERO, loadLibraryAddress, address, 0, (IntPtr)null) == INTPTR_ZERO)
+            {
+                return false;
+            }
+            CloseHandle(hndProc);
+            return true;
+        }
+    }
+}
--- YimInjector/MainWindow.xaml.cs
+++ YimInjector/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +21,75 @@ namespace YimInjector
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HttpClient client = new HttpClient();
+
+        private static string dllPath = @"C:\Windows\Temp\YimMenu.dll";
+        private static string downloadURL = "https://github.com/YimMenu/YimMenu/releases/download/nightly/YimMenu.dll";
+
+        private Process? GTAProcess = null;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+                {
+                    MessageBox.Show("Run YimInjector as Administrator!");
+                    Environment.Exit(-1);
+                }
+            }
+
+            
+        }
+
+        private async void injectButton_Click(object sender, RoutedEventArgs e)
+        {
+            injectButton.IsEnabled = false;
+
+            if (File.Exists(dllPath))
+            {
+                File.Delete(dllPath);
+            }
+
+            statusLabel.Content = "STATUS: Downloading latest YimMenu DLL...";
+
+            HttpResponseMessage response = await client.GetAsync(downloadURL);
+            using (FileStream fs = new FileStream(dllPath, FileMode.CreateNew, FileAccess.Write))
+            {
+                await response.Content.CopyToAsync(fs);
+            }
+
+            Process[] processes = Process.GetProcessesByName("GTA5");
+            if (processes.Length == 0)
+            {
+                statusLabel.Content = "STATUS: Run GTA first!";
+                injectButton.IsEnabled = true;
+                return;
+            }
+
+            Process process = processes[0];
+            bool status = Injector.Inject(process, dllPath);
+            if (!status)
+            {
+                MessageBox.Show("Injection failed");
+                Environment.Exit(-2);
+            }
+
+            statusLabel.Content = "STATUS: Injected!";
+
+            GTAProcess = process;
+
+            GTAProcess.EnableRaisingEvents = true;
+            GTAProcess.Exited += GTAProcess_Exited;
+        }
+
+        private void GTAProcess_Exited(object? sender, EventArgs e)
+        {
+            statusLabel.Content = "STATUS: Idle";
+            injectButton.IsEnabled = true;
         }
     }
 }
